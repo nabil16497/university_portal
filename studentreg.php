bd68204f781aedc6c4471ff418c210c0d7ba78ad
nabil16497 <nabil16497@gmail.com>
2020-08-08 03:50:59 +0600

    studentreg2

--- studentreg.php
+++ studentreg.php
@@ -2,20 +2,25 @@
 <html>
 <head>
 	<title></title>
+	<style>
+		td{
+			padding: 15px;
+		}
+	</style>
 </head>
 <body>
 <?php include('header1.php');?>
 
 <div style="border:3px solid #000000;border-radius: 10px; margin-top: 10px;margin-bottom: 10px;padding: auto;">
-	<div style="">
+	<div style="margin: auto; font-size: 15px;">
 		<form>
-			<table>
+			<table style="padding: 10px;">
 				<tr>
 					<td>
 						First Name:
 					</td>
 					<td>
-						
+						<input type="text" name="firstname"style="font-size: 15px;">
 					</td>
 				</tr>
 
@@ -24,7 +29,7 @@
 						Last Name:
 					</td>
 					<td>
-						
+						<input type="text" name="lastname"style="font-size: 15px;">
 					</td>
 				</tr>
 
@@ -33,7 +38,7 @@
 						Date of Birth:
 					</td>
 					<td>
-						
+						<input type="Date" name="dob"style="font-size: 15px;">
 					</td>
 				</tr>
 
@@ -42,7 +47,9 @@
 						Gender:
 					</td>
 					<td>
-						
+						<input type="radio" name="gender" value="male"> Male
+						<input type="radio" name="gender" value="female"> Female
+						<input type="radio" name="gender" value="other"> Other 
 					</td>
 				</tr>
 
@@ -51,7 +58,7 @@
 						Contact:
 					</td>
 					<td>
-						
+						<input type="text" name="contact"style="font-size: 15px;">
 					</td>
 				</tr>
 
@@ -60,7 +67,7 @@
 						Email:
 					</td>
 					<td>
-						
+						<input type="text" name="email"style="font-size: 15px;">
 					</td>
 				</tr>
 
@@ -69,7 +76,10 @@
 						Nationality:
 					</td>
 					<td>
-						
+						<select name="nationality"style="font-size: 15px;">
+						<option value=""></option>	
+						<option value="Bangladesh">Bangladesh</option>
+						</select>
 					</td>
 				</tr>
 
@@ -78,7 +88,7 @@
 						Address:
 					</td>
 					<td>
-						
+						<textarea name="address" rows="6" cols="50">Please Write Your Address Here-</textarea>
 					</td>
 				</tr>
 
@@ -87,7 +97,17 @@
 						Blood Group:
 					</td>
 					<td>
-						
+						<select name="blood"style="font-size: 15px;">
+						<option value=""></option>	
+						<option value="A+ve">A+ve</option>
+						<option value="A+ve">B+ve</option>
+						<option value="A+ve">AB+ve</option>
+						<option value="A+ve">O+ve</option>
+						<option value="A+ve">A-ve</option>
+						<option value="A+ve">B-ve</option>
+						<option value="A+ve">AB-ve</option>
+						<option value="A+ve">O-ve</option>
+						</select>
 					</td>
 				</tr>
 
@@ -96,7 +116,7 @@
 						Department:
 					</td>
 					<td>
-						
+						<input type="text" name="department"style="font-size: 15px;">
 					</td>
 				</tr>
 
