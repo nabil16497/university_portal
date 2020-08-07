0952fe1cf8ffe308c3d87cc8a950de5474d21818
nabil16497 <nabil16497@gmail.com>
2020-08-08 04:23:13 +0600

    studentreg4

--- studentreg.php
+++ studentreg.php
@@ -9,11 +9,58 @@
 	</style>
 </head>
 <body>
+
+<?php
+
+$pperror ="";
+	$picaddress = "userdp.png";
+	if(isset($_POST['submit'])){
+	$file = $_FILES['file'];
+
+	$fileName = $_FILES['file']['name'];
+	$fileTmpName = $_FILES['file']['tmp_name'];
+	$fileSize = $_FILES['file']['size'];
+	$fileError = $_FILES['file']['error'];
+	$fileType = $_FILES['file']['type'];
+
+	$fileExt = explode('.', $fileName);
+	$fileActualExt = strtolower(end($fileExt));
+
+
+	$allow = array('jpg','jpeg','png');
+	if(!empty($fileName)){
+	if (in_array($fileActualExt, $allow)) {
+		if($fileError ===0){
+			if($fileSize < 4194304){
+				$fileNameNew = uniqid('',true).".".$fileActualExt;
+				$fileDes = 'upload/'.$fileNameNew;
+				move_uploaded_file($fileTmpName, $fileDes);
+				$ppm = "Image sucessfully uploaded";
+				$picaddress = $fileDes;
+			}
+			else{
+				$pperror = "File size too large (Maximum file size- 4MB)";
+			}
+		}
+		else{
+			$pperror = "There was an error uploading your file!";
+		}
+	}
+	else{
+		$pperror = "Only images are allowed (jpeg, jpg, png)";
+	}
+}
+	else{
+		$pperror = "Please Select an image";
+	}
+ }
+
+ ?>
 <?php include('header1.php');?>
 
-<div style="border:3px solid #000000;border-radius: 10px; margin-top: 10px;margin-bottom: 10px;padding: auto;">
-	<div style="margin: auto; font-size: 15px;">
-		<form>
+<div style="border:3px solid #000000;border-radius: 10px; margin-top: 20px;margin-bottom: 10px;padding: auto;">
+	<div style="margin: auto; font-size: 15px;padding: 10px;">
+		<form action="changepp.php" method="POST" enctype="multipart/form-data">
 			<table style="padding: 10px;">
 				<tr>
 					<td>
@@ -22,6 +69,13 @@
 					<td>
 						<input type="text" name="firstname"style="font-size: 15px;">
 					</td>
+					<td rowspan="11">
+
+					<img src= <?php echo $picaddress; ?> alt="Profile Picture" style="max-height: 200px ; margin-bottom: 10px;"><br>
+  					<input type="file" name="file">
+  					<span style="color: #ff0000"><?php echo $pperror ?></span>
+					</td>
+
 				</tr>
 
 				<tr>
@@ -31,6 +85,7 @@
 					<td>
 						<input type="text" name="lastname"style="font-size: 15px;">
 					</td>
+					
 				</tr>
 
 				<tr>
@@ -128,8 +183,11 @@
 						<input type="text" name="program"style="font-size: 15px;">
 					</td>
 				</tr>
-
 			</table>
+
+			<hr>
+			<input type="submit" value="submit" name="submit" style ="margin-bottom: 10px; margin-left: 20px;">
+
 		</form>
 	</div>
 	
