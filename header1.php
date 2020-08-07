12ac4521b6e95c4df071f322bba0c72a52a46f6e
nabil16497 <nabil16497@gmail.com>
2020-08-07 13:48:28 +0600

    header

--- header1.php
+++ header1.php
@@ -2,8 +2,19 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/header.css">
 </head>
 <body>
+	<header>
+		<div id="logo">
+			<a href="home.php"><img src="logo.png" style="height: 80px" alt="logo"></a>
+		</div>
+		<ul id="nav">
+			<li style="border-right: 1px solid #000000;"><a href="">______</a></li>
+			<li style="border-right: 1px solid #000000;"><a href="">_____</a></li>
+			<li><a href="">_______</a></li>
+		</ul>
+	</header>
 
 </body>
 </html>
