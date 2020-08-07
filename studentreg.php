48ea1e69b7fcb3c1be29be3ca80c2c5fe5fdbbf3
nabil16497 <nabil16497@gmail.com>
2020-08-08 02:35:14 +0600

    studentreg

---
+++ studentreg.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php include('header1.php');?>
+
+<div style="border:3px solid #000000;border-radius: 10px; margin-top: 10px;margin-bottom: 10px;padding: auto;">
+	<div style="">
+		<form>
+			<table>
+				<tr>
+					<td>
+						First Name:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Last Name:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Date of Birth:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Gender:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Contact:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Email:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Nationality:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Address:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Blood Group:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Department:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						Program:
+					</td>
+					<td>
+						
+					</td>
+				</tr>
+
+			</table>
+		</form>
+	</div>
+	
+</div>
+<?php include('footer.php');?>
+
+</body>
+</html>
